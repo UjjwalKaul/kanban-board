f3a29fd9edd30d035a96c5475fbf45fe68d2fb7f
Ujjawal Kaul <ujjwal171@outlook.com>
2024-09-20 22:13:56 +0530

    Revert "Blended login and signup in one comp"
    This reverts commit c2fc10d1f0f413fdf9663da118cae749d06945c6.

--- myComponents/Login.tsx
+++ myComponents/Login.tsx
@@ -1,5 +1,5 @@
-'use client';
 import { Button } from '@/components/ui/button';
+import { useState } from 'react';
 import {
   Card,
   CardContent,
@@ -8,9 +8,9 @@ import {
   CardTitle,
 } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
-import Link from 'next/link';
 import { Label } from '@/components/ui/label';
-import { useState } from 'react';
+import Link from 'next/link';
+
 const Login = () => {
   const [isLogin, setIsLogin] = useState(true);
 
