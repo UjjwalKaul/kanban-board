13f446e6197723ca2c00dd6c92c482f055dc0d05
Ujjawal Kaul <ujjwal171@outlook.com>
2024-09-20 18:53:27 +0530

    Added home page layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,23 @@
+import Image from 'next/image';
+import image from './assets/image.png';
+
 export default function Home() {
-  return <div>Hello</div>;
+  return (
+    <div className="w-screen h-screen">
+      <div className="flex flex-col md:flex-row items-center justify-center h-full">
+        <div className="hidden md:block w-full md:w-1/2">
+          <Image
+            src={image}
+            alt="HomePage Image"
+            className="h-screen object-cover"
+            priority
+          />
+        </div>
+
+        <div className="w-full md:w-1/2 flex justify-center items-center">
+          <div className="text-center">SignUp or Login</div>
+        </div>
+      </div>
+    </div>
+  );
 }
